6e280863f9c7c9eeffe41b3175a4743d012263bc
Devteam <monicaacha2103@gmail.com>
2018-07-24 21:35:43 +0530

    first

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ class App extends Component {
 			selectedVideo : null
 		};
 		this.videoSearch('music');
-		//this.videoSearch = this.videoSearch.bind(this);
+		this.videoSearch = this.videoSearch.bind(this);
 
 
 
@@ -31,7 +31,7 @@ class App extends Component {
 	}	
 
 	render(){
-		const videoSearch = _.debounce((term) => { this.videoSeach(term) }, 300);
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
 
   	return (
    		<div>
